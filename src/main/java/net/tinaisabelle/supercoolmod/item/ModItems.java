71ae4da4b6148c3a1e69afb70fa5f97774fe7ac0
Isballr <isabelle@loodin.se>
2026-04-27 12:44:15 +0200

    sparar innan branch byte

--- src/main/java/net/tinaisabelle/supercoolmod/item/ModItems.java
+++ src/main/java/net/tinaisabelle/supercoolmod/item/ModItems.java
@@ -13,6 +13,8 @@ public class ModItems {
     //man lowkey ska lägga de som e food på en annan, för den e inte bara items o så men det är iaf test grej, ändrar mer sen när jag kommer dit
     public static final Item RAW_RICE = registerItem("raw_rice", new Item(new Item.Settings()));
 
+
+
     public static final Item COOKED_RICE = registerItem("cooked_rice", new Item(new Item.Settings().food(ModFoodComponents.COOKED_RICE)));
     public static final Item TOMATO = registerItem("tomato", new Item(new Item.Settings().food(ModFoodComponents.TOMATO)));
     public static final Item CUCUMBER = registerItem("cucumber", new Item(new Item.Settings().food(ModFoodComponents.CUCUMBER)));
